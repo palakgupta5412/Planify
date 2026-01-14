bda61a7cd5b75f131823b6eacb8c9af645f066a7
palakgupta5412 <palakgupta425@gmail.com>
2026-01-14 11:21:36 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+  origin: "*" ,
+  credentials: true,
 }));
 
 app.use(express.json(
